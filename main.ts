314e618b00ebdba0d863d6453af3dd1bf5adc5fc
SamA-davies-tech <72987262+SamA-davies-tech@users.noreply.github.com>
2020-11-02 19:24:02 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Monkey.x == 0) {
+        Monkey.vy = -70
+    }
+})
+let Monkey: Sprite = null
+tiles.setTilemap(tilemap`level`)
+Monkey = sprites.create(img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . f f 
+    c c c c c d d d e e f c . f e f 
+    . f d d d d d e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f e f 
+    . . . f e f f e f e e e e f f . 
+    . . . f e f f e f e e e e f . . 
+    . . . f d b f d b f f e f . . . 
+    . . . f d d c d d b b d f . . . 
+    . . . . f f f f f f f f f . . . 
+    `, SpriteKind.Player)
+Monkey.setFlag(SpriteFlag.StayInScreen, true)
+Monkey.y = 100
+game.onUpdate(function () {
+    Monkey.say("" + Math.round(Monkey))
+    Monkey.x += controller.dx()
+})
---
+++ tilemap.g.ts
@@ -0,0 +1,23 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`0a0008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010101010101010101`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.castle.tileGrass2], TileScale.Sixteen)
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
